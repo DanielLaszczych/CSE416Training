839c0e2f720023b5cb71859216f48bfd581f6a0d
DanielLaszczych <daniellaszczych@gmail.com>
2021-10-07 01:33:50 -0400

    Added database url for heroku

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(process.env.DATABASE_URL || MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
     return server.listen({ port: PORT });
