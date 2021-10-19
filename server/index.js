90a415eb58e4163b9d7c31f6410ffcab5ceb1d3a
DanielLaszczych <daniellaszczych@gmail.com>
2021-10-18 21:22:46 -0400

    Changed layout of typedefs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -12,36 +11,38 @@ const PORT = process.env.PORT || 5000;
 startApolloServer();
 
 async function startApolloServer() {
-  const app = express();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
-  await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    app.get('*', (request, response) => {
-      response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
     });
-  } else {
-    app.get('/', (req, res) => {
-      res.send('Hello World!');
-    });
-  }
 
-  mongoose
-    .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-    .then(() => {
-      console.log('MongoDB Connected');
-      return app.listen({ port: PORT });
-    })
-    .then((res) => {
-      console.log(`Server running at http://localhost:${PORT}`);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    await server.start();
+    server.applyMiddleware({ app, path: '/graphql' });
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('../client/build'));
+        app.get('*', (request, response) => {
+            response.sendFile(
+                path.join(__dirname, '../client/build', 'index.html')
+            );
+        });
+    } else {
+        app.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
+    }
+
+    mongoose
+        .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+        .then(() => {
+            console.log('MongoDB Connected');
+            return app.listen({ port: PORT });
+        })
+        .then((res) => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
